33de9afbbf7813df3de45dadc0ea4a86547f4eea
Василий Борцов <s.t.a.l.k.e.r.bva@gmail.com>
2022-11-13 18:46:21 +0300

    Третье задание

--- home4.py
+++ home4.py
@@ -22,3 +22,11 @@ while i <= num:
     else:
         i += 1
 print(lst)
+
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+lst = list(map(int, input().split()))
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(new_lst)
+
